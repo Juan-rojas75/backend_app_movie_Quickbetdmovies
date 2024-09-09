58c71227b8cbc6fd0ffecdb4449097194dc18ab9
Juan-rojas75 <71771380+Juan-rojas75@users.noreply.github.com>
2024-09-09 14:07:32 -0500

    Update movies.service.ts

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -18,8 +18,8 @@ export class MoviesService {
   async getPopularMovies(page: number) {
     let url_ = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}&page=${page}`
     const response = await this.httpService.get(url_).toPromise();
-    console.log(response.data)
-    return response.data;
+    console.log(response)
+    return response;
   }
 
   async getMoviesDetails(id: number) {
